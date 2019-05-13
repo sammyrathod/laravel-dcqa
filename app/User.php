7a1f10863964201385567e5d561a914827237b5b
Sammy Rathod <sammyrathod@gmail.com>
2019-05-13 20:47:30 +0530

    Generate Questions model via artisan model generatore

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Questions()
+    {
+        return $this->hasMany(Questions::class);
+    }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=str_slug($value);
+    }
 }
