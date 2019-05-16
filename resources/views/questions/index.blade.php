84e3e152af01a5bc8dc72d951998167778318b70
Sammy Rathod <sammyrathod@gmail.com>
2019-05-16 18:54:40 +0530

    Implement delete and showing functionality for the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -29,21 +29,21 @@
                             </div>
                         </div>
                         <div class="media-body">
-                            <div class="d-flex flex-column align-items-center"> 
+                            <div class="d-flex align-items-center"> 
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title}}</a></h3>
                                 <div class="ml-auto">
-                                <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a> 
-                                <form class="form-delete" method="post" action={{ route('questions.destroy', $question->id)}} >
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</buttom>
-                                </form>
+                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a> 
+                                    <form class="form-delete" method="post" action={{ route('questions.destroy', $question->id)}} >
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</buttom>
+                                    </form>
                                 </div>
                             </div>
                            <p class="lead">Asked by 
-                           <a href="{{ $question->user->url }}"> {{ $question->user->name }}</a>
-                           <small class="text-muted"> {{ $question->created_date }}</small>
-                            </p>
+                                <a href="{{ $question->user->url }}"> {{ $question->user->name }}</a>
+                                <small class="text-muted"> {{ $question->created_date }}</small>
+                           </p>
                             {{ str_limit($question->body,250) }} 
                         </div>
                     </div>
